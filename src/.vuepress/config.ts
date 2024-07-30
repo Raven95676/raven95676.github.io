5b5158a036679fd40188ddad8d5b18aff81a22f5
Raven95676 <Raven95676@proton.me>
2024-07-30 17:33:50 +0800

    增加googleAnalytics功能

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,4 +1,6 @@
 import { defineUserConfig } from "vuepress";
+import { googleAnalyticsPlugin } from '@vuepress/plugin-google-analytics'
+
 
 
 import theme from "./theme.js";
@@ -11,6 +13,12 @@ export default defineUserConfig({
   description: "这里是吧友们可能会问的问题，非病毒吧官方网站。",
 
   theme,
+
+  plugins: [
+    googleAnalyticsPlugin({
+      id: 'G-SCCND9C7KZ',
+    }),
+  ],
 
   // 和 PWA 一起启用
   // shouldPrefetch: false,
