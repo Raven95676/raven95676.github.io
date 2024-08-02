e2e1b44a4dd9428d8e01efa6855d6477a0525fca
Raven95676 <Raven95676@proton.me>
2024-08-03 00:17:14 +0800

    添加网站关键词；在“通过良好的上网习惯防病毒”中添加暂时不知道放哪里的杂项；修改情绪化表达

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -11,7 +11,10 @@ export default defineUserConfig({
   lang: "zh-CN",
   title: "非官方病毒吧Windows FAQ",
   description: "这里是吧友们可能会问的问题，非病毒吧官方网站。",
-  head: [['meta',{'name':'google-site-verification','content':'1YEe1_R0CLvOH_wRVjwtCU0jZ-KnLk7KubhPM9kiaV0'}]],
+  head: [
+    ['meta',{'name':'google-site-verification','content':'1YEe1_R0CLvOH_wRVjwtCU0jZ-KnLk7KubhPM9kiaV0'}],
+    ['meta', { name: 'keywords', content: '计算机安全,小白教程,教程,计算机'}]
+  ],
 
   theme,
 
